dcb340ea45e873ce801d82718cd1ee800e795eec
Ph4kh1n <phakhinnongthong@gmail.com>
2023-05-23 23:01:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let imgBx = document.querySelectorAll('.imgBx');
+let contentBx = document.querySelectorAll('.contentBx');
+
+for (let i = 0; i < imgBx.length; i++) {
+    imgBx[i].addEventListener( 'click', function(){
+        for(let i = 0; i < contentBx.length; i++) {
+            contentBx[i].className = 'contentBx';
+        }
+
+        document.getElementById(this.dataset.id).className = 'contentBx active';
+
+        for (let i = 0; i < imgBx.length; i++) {
+             imgBx[i].className = 'imgBx';
+        }
+
+        this.className = 'imgBx active';
+        
+    })
+}
